df2883aaf6e223cbd1caf66c481654d481423e8b
unknown <macrowell@yahoo.com>
2022-11-10 16:49:25 -0500

    creates footer

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,17 +1,11 @@
-import github from './Github - Original.svg';
-import linkedin from './Linkedin - Original.svg';
-import menu from './Menu.svg';
-import close from './Close_round.svg';
-import projectsPic from './projectsPic.png';
-import experiencePic from './experiencePic.png';
-import contactPic from './contactPic.png';
+import github from './Github - Original.svg'
+import linkedin from './LinkedIn - Original.svg'
+import menu from './Menu.svg'
+import close from './Close_round.svg'
 
 export {
     github,
     linkedin,
     menu,
     close,
-    projectsPic,
-    experiencePic,
-    contactPic
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import { github, linkedin } from '../assets'
+
 const Footer = () => {
   return (
     <div id='footer'>
-      Footer
+      <div className='contacts-container'>
+        <a href='https://www.linkedin.com/in/macrowell/' target='_blank'><img src={linkedin} alt='LinkedIn ' /></a>
+          <div className='contacts'>
+            <span>macrowell@yahoo.com</span>
+            <span>{'(248) 860-9852'}</span>
+          </div>
+        <a href='https://github.com/macrowell70' target='_blank'><img src={github} alt='GitHub' /></a>
+      </div>
     </div>
   )
 }
