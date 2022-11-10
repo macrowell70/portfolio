7f114b516fb410c726a4af30a9df6f66a2cc8ab9
unknown <macrowell@yahoo.com>
2022-11-10 18:09:56 -0500

    creates modal for projects

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const Modal = () => {
+const Modal = props => {
+    const { project } = props
+
+    const handleModalClose = () => {
+        return props.handleModalClose(false)
+    }
+
   return (
-    <div id='modal'>
-      Modal
+    <div id='modal' className='modal-container'>
+        <div className='modal-card'>
+            <button className='modal-close' onClick={handleModalClose}>x</button>
+            <div className='text-container'>
+                <div className='modal-title'>{project.title}</div>
+                <p className='modal-description'>{`${project.description}`}</p>
+                <a href={`${project.link}`} target='_blank' rel='noreferrer'>View Site</a> 
+            </div>
+            
+        </div>
     </div>
   )
 }
