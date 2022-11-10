f6650bbbeb27158b926434da086ad6adb8a7c24e
unknown <macrowell@yahoo.com>
2022-11-10 14:21:25 -0500

    creates introduction animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Card } from './index'
+import { intro, navCardInfo} from '../constants'
 
 const Home = () => {
   return (
     <div id='home'>
-      Home
+      {intro.map(text => (
+        <h2 key={text.id} className={`intro text-${text.id}`}>
+          {text.text}
+        </h2>
+      ))}
     </div>
   )
 }
