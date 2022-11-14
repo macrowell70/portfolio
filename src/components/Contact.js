fdeca23cf7a7713e8d090494759aaa2dee3758bd
unknown <macrowell@yahoo.com>
2022-11-14 15:39:35 -0500

    removes contact form to be implemented at a later date

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,63 +1,12 @@
-import React, { useState } from 'react'
-import { formFields, initialFormValues } from '../constants'
+import React from 'react'
 
 const Contact = () => {
-  const [formValues, setFormValues] = useState(initialFormValues)
-
-  const handleChange = evt => {
-    const { name, value } = evt.target
-    evt.preventDefault()
-    setFormValues({...formValues, [name]: value})
-  }
-
-  const handleSubmit = () => {
-    setFormValues(initialFormValues)
-  }
-
-  const handleReset = () => {
-    setFormValues(initialFormValues)
-  }
 
   return (
     <div id='contact'>
       <h2 className='contact-header'>Contact Me</h2>
-      <div className='form-container'>
-        <form className='contact-form' onSubmit={handleSubmit}>
-          {formFields.map(field => (
-            <input
-              key={field.name}
-              id={field.name}
-              className='form-field'
-              placeholder={field.placeholder}
-              name={field.name}
-              type={field.type}
-              value={formValues[field.name]}
-              onChange={evt => handleChange(evt)}
-            />
-          ))}
-          <textarea 
-            id='message'
-            className='form-field'
-            placeholder='Message*'
-            name='message'
-            value={formValues.message}
-            onChange={evt => handleChange(evt)}
-          />
-          <div className='button-container'>
-            <input 
-              type='submit' 
-              value='Send Message'
-              className='contact-form-submit' 
-            />
-            <input
-              type='button'
-              value='Reset Form'
-              className='contact-form-reset'
-              onClick={handleReset}
-            />
-          </div>
-        </form>
-      </div>
+      <p>Please feel free to email or call me at the contact information provided below, or click to icons to check out my other pages. You can read all about me on LinkedIn, or see my GitHub account to see what I have been working on and even view the source code for this portfolio page.</p><br></br>
+      <p>I hope to hear from you soon!</p>
     </div>
   )
 }
