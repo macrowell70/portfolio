1f14427b70ef70fb8dcc96363d02f08f1a173c12
unknown <macrowell@yahoo.com>
2022-11-15 08:26:24 -0500

    creates navigation menu for mobile page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React, { useState, useEffect } from 'react'
 import './App.scss';
 
 import { Nav, Home, Projects, About, Contact, Footer } from './components';
 
+const mediaMatch = window.matchMedia("(min-width: 768px)")
+
 function App() {
+  const [matches, setMatches] = useState(mediaMatch.matches)
+
+  useEffect(() => {
+    mediaMatch.addEventListener('change', evt => setMatches(evt.matches))
+  }, [])
+
   return (
     <div id='main'>
       <div className='home-container'>
-        <Nav />
+        <Nav matches={matches} />
         <Home />
         <Projects />
         <About />
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { navLinks } from '../constants'
+import { menu, close } from '../assets'
 
-const Nav = () => {
+const Nav = (props) => {
+  const [menuOpen, setMenuOpen] = useState(false)
+  const { matches } = props
+
+  const handleMenuOpen = () => {
+    setMenuOpen(prev => !prev)
+  }
+
   return (
     <nav id='nav'>
+      {matches && 
       <ul className='navLinks'>
         {navLinks.map(link => (
           <li className='navLink' key={link.id}>
             <a href={`#${link.id}`}>{link.title}</a>
           </li>
         ))}
-      </ul> 
+      </ul>}
+      {!matches &&
+      <div className='mobile-nav-container'>
+        <img 
+          src={!menuOpen ? menu : close} 
+          alt="menu icon" 
+          onClick={handleMenuOpen} 
+        />
+        {menuOpen &&
+          <ul className='navLinks-mobile'>
+          {navLinks.map(link => (
+            <li className='navLink-mobile' key={link.id}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>}
+      </div>}
     </nav>
   )
 }
